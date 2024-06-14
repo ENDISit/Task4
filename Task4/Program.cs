938ee4708c998746de2d22ec78b7e52941e1d924
ENDISit <mr.ilia08@mail.ru>
2024-06-14 22:29:24 +0300

    Добавьте файлы проекта.

---
+++ Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите сумму вклада: ");
+        double deposit = Convert.ToDouble(Console.ReadLine());
+        double interestRate;
+
+        if (deposit < 100)
+        {
+            interestRate = 0.05;
+        }
+        else if (deposit >= 100 && deposit <= 200)
+        {
+            interestRate = 0.07;
+        }
+        else
+        {
+            interestRate = 0.10;
+        }
+
+        double finalAmount = deposit + (deposit * interestRate);
+        Console.WriteLine($"Сумма вклада с процентами: {finalAmount}");
+
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
